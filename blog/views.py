de02b8306cfb7571063831a974b9d9035946e211
Marcin Weglowski <marcin@escali.no>
2019-11-26 10:21:08 +0100

    User html templates

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'blog/about.html')
