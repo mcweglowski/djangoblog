78757f9a9facfd6399c60c866a805456a02e0236
Marcin Weglowski <marcin@escali.no>
2019-12-05 16:06:24 +0100

    Use ListView instead of function to generate request

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 from .models import Post
 
-def home(request):
-    context = {'posts' : Post.objects.all()}
-    return render(request, 'blog/home.html', context)
+#def home(request):
+#    context = {
+#        'posts' : Post.objects.all()
+#    }
+#    return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 def about(request):
     return render(request, 'blog/about.html', {'title' : 'About'})
